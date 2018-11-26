a82fec97d756e5a2d1e6ab07c4a39497898ab210
Blackscure <Wekesabuyahi@gmail.com>
2018-11-26 16:43:39 +0300

    models

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
+from database import Database
 from models.post import Post
 
-post = Post("Post1 title", "Post1 content", "Post1 author")
-post2 = Post("Post2 title", "Post2 content", "Post2 author")
 
-print(post.content)
+Database.initialize()
 
-print(post2.content)
+
+post = Post.from_mongo('ab8e363a2f9140298571cb3f0ee02008')
+
+print(post)
