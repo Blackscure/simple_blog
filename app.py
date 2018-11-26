04e4188ee19fdb4105bbf90476f400287fbd58d6
Blackscure <Wekesabuyahi@gmail.com>
2018-11-26 11:45:15 +0300

    models

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
-import pymongo
+from models.post import Post
 
-uri = "mongodb://127.0.0.1:27017"
-client = pymongo.MongoClient(uri)
-database = client["fullstack"]
-collection = database["students"]
+post = Post()
+post2 = Post()
 
-students = [student for student in collection.find({})]
+print(post.content)
 
-print(students)
+print(post2.content)
